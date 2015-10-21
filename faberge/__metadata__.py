08ad4d6018f5c45e17a77aa3f8bfff8170a4dc0d
Trevor Bača <trevorbaca@gmail.com>
2015-10-21 12:25:40 -0400

    Added score directories.

---
+++ faberge/__metadata__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from abjad import *
+
+
+metadata = datastructuretools.TypedOrderedDict(
+    [
+        ('title', 'Enquête Fabergé'),
+        ('year', 2015),
+        ]
+    )
---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+
+install_requires = (
+    'abjad',
+    )
+
+def main():
+    setup(
+        author='Trevor Bača',
+        author_email='trevor.baca@gmail.com',
+        install_requires=install_requires,
+        name='faberge',
+        packages=('faberge',),
+        url='https://github.com/trevorbaca/faberge',
+        version='0.1',
+        zip_safe=False,
+        )
+
+if __name__ == '__main__':
+    main()
