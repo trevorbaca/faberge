a34cc6dfa18eb75e928c4e28a8ccc6f1121d78d4
Trevor Bača <trevor.baca@gmail.com>
2016-04-12 21:33:48 -0500

    Time.

---
+++ faberge/tools/make_electricity_rhythm_specifier.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import baca
+from abjad.tools import rhythmmakertools
+
+
+def make_electricity_rhythm_specifier():
+    return baca.rhythm.make_messiaen_tied_note_rhythm_specifier()
