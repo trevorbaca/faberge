85f69e61d8474e2d9c354d178896c8c229c151dd
Trevor Bača <trevor.baca@gmail.com>
2016-04-12 21:52:22 -0500

    Time.

--- faberge/segments/segment_j/__metadata__.py
+++ faberge/segments/segment_j/__metadata__.py
@@ -4,6 +4,7 @@ from abjad import *
 
 metadata = datastructuretools.TypedOrderedDict(
     [
+        ('first_bar_number', 535),
         ('name', 'J'),
         ('segment_count', 12),
         ('segment_number', 11),
