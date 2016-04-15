5e4290cea22e7e8eb7abcedba431cdd3f0d8666f
Trevor Bača <trevor.baca@gmail.com>
2016-04-15 11:16:57 -0500

    Color.

--- faberge/segments/segment_k/__metadata__.py
+++ faberge/segments/segment_k/__metadata__.py
@@ -4,7 +4,7 @@ from abjad import *
 
 metadata = datastructuretools.TypedOrderedDict(
     [
-        ('first_bar_number', 571),
+        ('first_bar_number', 573),
         ('name', 'K'),
         ('segment_count', 12),
         ('segment_number', 12),
