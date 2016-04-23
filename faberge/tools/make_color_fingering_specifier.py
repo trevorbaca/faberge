3fb18109195b64be2b541f24f2cb4bc6a4d69b2a
Trevor Bača <trevor.baca@gmail.com>
2016-04-23 13:46:58 -0500

    Ported color fingerings forward.

--- faberge/tools/make_color_fingering_specifier.py
+++ faberge/tools/make_color_fingering_specifier.py
@@ -4,7 +4,6 @@ import baca
 
 def make_color_fingering_specifier():
     color_fingering_specifier = baca.tools.ColorFingeringSpecifier(
-        apply_to_every_note=True,
         deposit_annotations=['color fingering'],
         number_lists=(
             [0, 1, 2, 1, 0, 1, 0, 1, 2, 1, 2, 1],
