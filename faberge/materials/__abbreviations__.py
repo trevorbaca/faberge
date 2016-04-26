9bc7682feb7a5db1d873fc58bdd9d0a0ecc7acf3
Trevor Bača <trevor.baca@gmail.com>
2016-04-26 13:30:23 -0500

    Updated abbreviations.

--- faberge/materials/__abbreviations__.py
+++ faberge/materials/__abbreviations__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from baca.__abbreviations__ import stages
-from baca.__abbreviations__ import stage_leaves
 from abjad.tools.rhythmmakertools import silence
 from abjad.tools.rhythmmakertools import silence_all
 from abjad.tools.rhythmmakertools import silence_every
