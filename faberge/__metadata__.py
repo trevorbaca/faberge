c09ecc6a191d5243a81951b1c9a836853bcc0e4f
Trevor Bača <trevor.baca@gmail.com>
2016-08-03 10:41:21 -0500

    Updated catalog number.

--- faberge/__metadata__.py
+++ faberge/__metadata__.py
@@ -4,7 +4,7 @@ import abjad
 
 metadata = abjad.datastructuretools.TypedOrderedDict(
     [
-        ('catalog_number', 'AWN-018'),
+        ('catalog_number', 'AWN-015'),
         ('composer', 'Ba\xc4\x8da'),
         ('forces_tagline', 'for eight players'),
         ('paper_size', '17 x 11 in'),
