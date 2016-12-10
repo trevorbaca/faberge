f5c2e97180dc5238494259cfea85469613986f35
Trevor Bača <trevor.baca@gmail.com>
2016-12-10 14:37:56 -0600

    Cleaned up import statements.

--- faberge/materials/tempi/definition.py
+++ faberge/materials/tempi/definition.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 import abjad
-import collections
 
 
-tempi = collections.OrderedDict([
+tempi = abjad.datastructuretools.TypedOrderedDict([
     (
         41,
         abjad.Tempo(abjad.Duration(1, 4), 41),
