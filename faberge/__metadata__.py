6e73ab6dc2cf505824588dd099d75f9ccd6c5fa7
Trevor Bača <trevor.baca@gmail.com>
2017-01-02 10:47:46 -0600

    Updated metadata.

--- faberge/__metadata__.py
+++ faberge/__metadata__.py
@@ -5,7 +5,7 @@ import abjad
 metadata = abjad.datastructuretools.TypedOrderedDict(
     [
         ('catalog_number', 'AWN-015'),
-        ('composer', 'Ba\xc4\x8da'),
+        ('composer', 'Bača'),
         ('forces_tagline', 'for eight players'),
         ('paper_size', '17 x 11 in'),
         ('price', '\\$80 / \\euro 72'),
