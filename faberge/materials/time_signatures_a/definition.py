81270f5b4d85b06f7a3b09cff9c8844152490d53
Trevor Bača <trevor.baca@gmail.com>
2017-01-29 10:31:20 -0600

    Updated flatten transforms.

--- faberge/materials/time_signatures_a/definition.py
+++ faberge/materials/time_signatures_a/definition.py
@@ -7,7 +7,7 @@ numerators = [[4, 6, 6], [4, 7], [3, 4, 6]]
 numerator_groups = baca.transforms.helianthate(numerators, -1, 1)
 assert len(numerator_groups) == 18, repr(len(numerator_groups))
 lengths = [len(_) for _ in numerator_groups]
-numerators = abjad.sequencetools.flatten_sequence(numerator_groups)
+numerators = baca.Sequence(numerator_groups).flatten()
 time_signatures_a = [abjad.TimeSignature((_, 4)) for _ in numerators]
 time_signature_groups = abjad.sequencetools.partition_sequence_by_counts(
     time_signatures_a,
--- faberge/materials/time_signatures_b/definition.py
+++ faberge/materials/time_signatures_b/definition.py
@@ -7,7 +7,7 @@ numerators = [[3, 4, 4], [2, 3], [2, 3, 4]]
 numerator_groups = baca.transforms.helianthate(numerators, -1, 1)
 assert len(numerator_groups) == 18, repr(len(numerator_groups))
 lengths = [len(_) for _ in numerator_groups]
-numerators = abjad.sequencetools.flatten_sequence(numerator_groups)
+numerators = baca.Sequence(numerator_groups).flatten()
 time_signatures_a = [abjad.TimeSignature((_, 4)) for _ in numerators]
 time_signature_groups = abjad.sequencetools.partition_sequence_by_counts(
     time_signatures_a,
