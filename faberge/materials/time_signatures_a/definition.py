39a584508531199903ac2bb9d32485f36a6ed74b
Trevor Bača <trevor.baca@gmail.com>
2017-01-29 19:45:36 -0600

    Updated partitions.

--- faberge/materials/time_signatures_a/definition.py
+++ faberge/materials/time_signatures_a/definition.py
@@ -9,8 +9,7 @@ assert len(numerator_groups) == 18, repr(len(numerator_groups))
 lengths = [len(_) for _ in numerator_groups]
 numerators = baca.Sequence(numerator_groups).flatten()
 time_signatures_a = [abjad.TimeSignature((_, 4)) for _ in numerators]
-time_signature_groups = abjad.sequencetools.partition_sequence_by_counts(
-    time_signatures_a,
+time_signature_groups = baca.Sequence(time_signatures_a).partition_by_counts(
     lengths,
     )
 time_signatures_a = time_signature_groups
