a1caf792b6fb2f7ff638b64c2cb909ab0a8b46df
Trevor Bača <trevor.baca@gmail.com>
2017-04-29 19:51:19 -0500

    Cleaned up package initializer.

--- faberge/__init__.py
+++ faberge/__init__.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
+import distutils.version
+import platform
+if not (
+    distutils.version.LooseVersion('3.5') <
+    distutils.version.LooseVersion(platform.python_version())
+    ):
+    raise ImportError('Requires Python 3.5.')
+del distutils
+del platform
 from faberge import tools
 from faberge.tools import MarkupLibrary as markup
 from faberge import materials
