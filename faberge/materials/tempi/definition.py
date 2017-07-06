efea5d1e2a07e21862f72f72cb88604e1a3943de
Trevor Bača <trevor.baca@gmail.com>
2017-07-06 09:29:01 -0500

    Changed Tempo to MetronomeMark.

--- faberge/materials/tempi/definition.py
+++ faberge/materials/tempi/definition.py
@@ -5,30 +5,30 @@ import abjad
 tempi = abjad.TypedOrderedDict([
     (
         41,
-        abjad.Tempo(abjad.Duration(1, 4), 41),
+        abjad.MetronomeMark(abjad.Duration(1, 4), 41),
         ),
     (
         52,
-        abjad.Tempo(abjad.Duration(1, 4), 52),
+        abjad.MetronomeMark(abjad.Duration(1, 4), 52),
         ),
     (
         64,
-        abjad.Tempo(abjad.Duration(1, 4), 64),
+        abjad.MetronomeMark(abjad.Duration(1, 4), 64),
         ),
     (
         80,
-        abjad.Tempo(abjad.Duration(1, 4), 80),
+        abjad.MetronomeMark(abjad.Duration(1, 4), 80),
         ),
     (
         100,
-        abjad.Tempo(abjad.Duration(1, 4), 100),
+        abjad.MetronomeMark(abjad.Duration(1, 4), 100),
         ),
     (
         125,
-        abjad.Tempo(abjad.Duration(1, 4), 125),
+        abjad.MetronomeMark(abjad.Duration(1, 4), 125),
         ),
     (
         156,
-        abjad.Tempo(abjad.Duration(1, 4), 156),
+        abjad.MetronomeMark(abjad.Duration(1, 4), 156),
         ),
     ])
