16de3aa75b64057996641ce4d84af0953a684ef0
Trevor Bača <trevor.baca@gmail.com>
2017-09-23 14:48:19 -0500

    Renamed segments.

---
+++ faberge/segments/A/__metadata__.py
@@ -0,0 +1,44 @@
+import abjad
+
+
+metadata = abjad.TypedOrderedDict(
+    [
+        (
+            'end_clefs_by_staff',
+            abjad.TypedOrderedDict(
+                [
+                    ('Cello Music Staff', None),
+                    ('Clarinet Music Staff', None),
+                    ('English Horn Music Staff', None),
+                    ('Flute Music Staff', None),
+                    ('Percussion Music Staff', 'treble'),
+                    ('Piano LH Music Staff', 'bass'),
+                    ('Piano RH Music Staff', None),
+                    ('Viola Music Staff', None),
+                    ('Violin Music Staff', None),
+                    ]
+                ),
+            ),
+        (
+            'end_instruments_by_context',
+            abjad.TypedOrderedDict(
+                [
+                    ('Cello Music Staff', 'cello'),
+                    ('Clarinet Music Staff', 'bass clarinet'),
+                    ('English Horn Music Staff', 'English horn'),
+                    ('Flute Music Staff', 'bass flute'),
+                    ('Percussion Music Staff', 'percussion'),
+                    ('Piano Staff Group', 'piano'),
+                    ('Viola Music Staff', 'viola'),
+                    ('Violin Music Staff', 'violin'),
+                    ]
+                ),
+            ),
+        ('end_tempo', 100),
+        ('end_time_signature', '4/4'),
+        ('first_bar_number', 5),
+        ('measure_count', 92),
+        ('segment_count', 12),
+        ('segment_number', 2),
+        ]
+    )
