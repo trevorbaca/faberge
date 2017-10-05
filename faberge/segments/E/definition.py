dd7687cf352e9b0a81c919bb738f737812076f8c
Trevor Bača <trevor.baca@gmail.com>
2017-10-05 09:59:05 -0500

    Integrated HorizontalSpacingSpecifier.

--- faberge/segments/E/definition.py
+++ faberge/segments/E/definition.py
@@ -116,7 +116,7 @@ maker = baca.TimeSignatureMaker(
     )
 measures_per_stage, metronome_mark_measure_map, time_signatures = maker()
 
-spacing_specifier = baca.HorizontalSpacingCommand(
+spacing_specifier = baca.HorizontalSpacingSpecifier(
     fermata_measure_width=abjad.Duration(1, 4),
     minimum_width=abjad.Duration(1, 12),
     )
