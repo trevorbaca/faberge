51f8c34bf1e407c1e4f867b7bb745e98b9be805e
Trevor Bača <trevor.baca@gmail.com>
2017-10-07 10:40:26 -0500

    Integrated f-strings.

--- faberge/tools/make_dal_niente_hairpins.py
+++ faberge/tools/make_dal_niente_hairpins.py
@@ -2,8 +2,7 @@ import baca
 
 
 def make_dal_niente_hairpins(stop_dynamic):
-    descriptor = 'niente < {}'
-    descriptor = descriptor.format(stop_dynamic)
+    descriptor = f'niente < {stop_dynamic}'
     return baca.hairpins(
         [descriptor],
         #include_following_rests=True,
