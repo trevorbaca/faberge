597905c34c6810e5f2ce80d72678b328e84596f9
Trevor Bača <trevor.baca@gmail.com>
2017-10-12 17:30:54 -0500

    Integrated hairpin targets.

--- faberge/tools/make_dal_niente_hairpins.py
+++ faberge/tools/make_dal_niente_hairpins.py
@@ -5,7 +5,7 @@ def make_dal_niente_hairpins(stop_dynamic):
     descriptor = f'niente < {stop_dynamic}'
     return baca.hairpins(
         [descriptor],
-        #include_following_rests=True,
         include_rests=True,
         selector=baca.select_leaves(leak=Right),
+        target=baca.select_leaves(),
         )
--- faberge/tools/make_increasing_dal_niente_hairpins.py
+++ faberge/tools/make_increasing_dal_niente_hairpins.py
@@ -11,7 +11,7 @@ def make_increasing_dal_niente_hairpins():
     descriptors += 20 * ['niente < f']
     return baca.hairpins(
         descriptors,
-        #include_following_rests=True,
         include_rests=True,
         selector=baca.select_leaves(leak=Right),
+        target=baca.select_leaves(),
         )
