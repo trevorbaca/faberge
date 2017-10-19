abc20ea6cbd11ca993e77a4bd4f26b671ac99416
Trevor Bača <trevor.baca@gmail.com>
2017-10-19 16:18:23 -0500

    Integrated hairpin changes.

--- faberge/segments/A/definition.py
+++ faberge/segments/A/definition.py
@@ -911,7 +911,7 @@ segment_maker(
     baca.transition_spanner(
         baca.markup.tasto(),
         baca.markup.pochiss_pont(),
-        selector=baca.select().leaves(leak=abjad.Right).wrap(),
+        selector=baca.select().rleaves().wrap(),
         ),
     )
 
@@ -921,7 +921,7 @@ segment_maker(
     baca.transition_spanner(
         None,
         baca.markup.tasto(),
-        selector=baca.select().leaves(leak=abjad.Right).wrap(),
+        selector=baca.select().rleaves().wrap(),
         ),
     )
 
@@ -930,7 +930,7 @@ segment_maker(
     baca.transition_spanner(
         None,
         baca.markup.pochiss_pont(),
-        selector=baca.select().leaves(leak=abjad.Right).wrap(),
+        selector=baca.select().rleaves().wrap(),
         ),
     )
 
@@ -938,7 +938,7 @@ segment_maker(
     baca.scope('Viola Music Voice', 3),
     baca.hairpins(
         ['pp < mp'],
-        selector=baca.select().leaves(leak=abjad.Right).wrap(),
+        selector=baca.select().rleaves().wrap(),
         ),
     )
 
@@ -948,7 +948,7 @@ segment_maker(
     baca.transition_spanner(
         None,
         baca.markup.tasto(),
-        selector=baca.select().leaves(leak=abjad.Right).wrap(),
+        selector=baca.select().rleaves().wrap(),
         ),
     )
 
@@ -957,7 +957,7 @@ segment_maker(
     baca.transition_spanner(
         None,
         baca.markup.pochiss_pont(),
-        selector=baca.select().leaves(leak=abjad.Right).wrap(),
+        selector=baca.select().rleaves().wrap(),
         ),
     )
 
@@ -965,7 +965,7 @@ segment_maker(
     baca.scope('Viola Music Voice', 6),
     baca.hairpins(
         ['pp < mp'],
-        selector=baca.select().leaves(leak=abjad.Right).wrap(),
+        selector=baca.select().rleaves().wrap(),
         ),
     )
 
@@ -975,7 +975,7 @@ segment_maker(
     baca.transition_spanner(
         None,
         baca.markup.tasto(),
-        selector=baca.select().leaves(leak=abjad.Right).wrap(),
+        selector=baca.select().rleaves().wrap(),
         ),
     )
 
@@ -984,7 +984,7 @@ segment_maker(
     baca.transition_spanner(
         None,
         baca.markup.pochiss_pont(),
-        selector=baca.select().leaves(leak=abjad.Right).wrap(),
+        selector=baca.select().rleaves().wrap(),
         ),
     )
 
@@ -992,7 +992,7 @@ segment_maker(
     baca.scope('Viola Music Voice', 9),
     baca.hairpins(
         ['pp < mp'],
-        selector=baca.select().leaves(leak=abjad.Right).wrap(),
+        selector=baca.select().rleaves().wrap(),
         ),
     )
 
@@ -1002,7 +1002,7 @@ segment_maker(
     baca.transition_spanner(
         None,
         baca.markup.tasto(),
-        selector=baca.select().leaves(leak=abjad.Right).wrap(),
+        selector=baca.select().rleaves().wrap(),
         ),
     )
 
@@ -1028,7 +1028,7 @@ segment_maker(
     baca.scope('Viola Music Voice', 14),
     baca.hairpins(
         ['pp < mp'],
-        selector=baca.select().leaves(leak=abjad.Right).wrap(),
+        selector=baca.select().rleaves().wrap(),
         ),
     )
 
@@ -1062,12 +1062,12 @@ segment_maker(
     baca.scope('Cello Music Voice', 1, 2),
     baca.hairpins(
         ['p < f'],
-        selector=baca.select().leaves(leak=abjad.Right).wrap(),
+        selector=baca.select().rleaves().wrap(),
         ),
     baca.transition_spanner(
         baca.markup.tasto_plus_poco_vib(),
         baca.markup.PO_plus_poco_vib(True, False),
-        selector=baca.select().leaves(leak=abjad.Right).wrap(),
+        selector=baca.select().rleaves().wrap(),
         ),
     )
 
@@ -1075,12 +1075,12 @@ segment_maker(
     baca.scope('Cello Music Voice', 4, 6),
     baca.hairpins(
         ['f > p'],
-        selector=baca.select().leaves(leak=abjad.Right).wrap(),
+        selector=baca.select().rleaves().wrap(),
         ),
     baca.transition_spanner(
         baca.markup.PO_plus_poco_vib(False, False),
         baca.markup.tasto_plus_poco_vib(True, False),
-        selector=baca.select().leaves(leak=abjad.Right).wrap(),
+        selector=baca.select().rleaves().wrap(),
         ),
     )
 
@@ -1088,12 +1088,12 @@ segment_maker(
     baca.scope('Cello Music Voice', 9, 10),
     baca.hairpins(
         ['p < ff'],
-        selector=baca.select().leaves(leak=abjad.Right).wrap(),
+        selector=baca.select().rleaves().wrap(),
         ),
     baca.transition_spanner(
         baca.markup.tasto_plus_poco_vib(False, False),
         baca.markup.poco_pont_plus_vib_mod(True, True),
-        selector=baca.select().leaves(leak=abjad.Right).wrap(),
+        selector=baca.select().rleaves().wrap(),
         ),
     )
 
@@ -1107,12 +1107,12 @@ segment_maker(
     baca.scope('Cello Music Voice', 14, 15),
     baca.hairpins(
         ['ff > pp'],
-        selector=baca.select().leaves(leak=abjad.Right).wrap(),
+        selector=baca.select().rleaves().wrap(),
         ),
     baca.transition_spanner(
         baca.markup.poco_pont_plus_sub_vib_mod(False, True),
         baca.markup.tasto_plus_non_vib(True, True),
-        selector=baca.select().leaves(leak=abjad.Right).wrap(),
+        selector=baca.select().rleaves().wrap(),
         ),
     )
 
@@ -1120,11 +1120,11 @@ segment_maker(
     baca.scope('Cello Music Voice', 16),
     baca.hairpins(
         ['pp < p'],
-        selector=baca.select().leaves(leak=abjad.Right).wrap(),
+        selector=baca.select().rleaves().wrap(),
         ),
     baca.transition_spanner(
         None,
         baca.markup.tasto_plus_poco_vib(False, True),
-        selector=baca.select().leaves(leak=abjad.Right).wrap(),
+        selector=baca.select().rleaves().wrap(),
         ),
     )
--- faberge/tools/dal_niente_hairpins.py
+++ faberge/tools/dal_niente_hairpins.py
@@ -6,5 +6,5 @@ def dal_niente_hairpins(stop_dynamic):
     descriptor = f'niente < {stop_dynamic}'
     return baca.hairpins(
         [descriptor],
-        selector=baca.select().leaves(leak=abjad.Right).wrap(),
+        selector=baca.select().rleaves().wrap(),
         )
