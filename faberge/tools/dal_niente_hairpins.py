dbb171258016968540b13ef7eadd7aceb0f0164d
Trevor Bača <trevor.baca@gmail.com>
2017-11-04 09:54:58 -0500

    Integrated selector changes.

--- faberge/tools/dal_niente_hairpins.py
+++ faberge/tools/dal_niente_hairpins.py
@@ -3,6 +3,6 @@ import baca
 
 def dal_niente_hairpins(stop):
     return baca.map(
-        baca.hairpin(f'niente < {stop}', baca.select().leaves()),
-        baca.select().rruns(),
+        baca.hairpin(f'niente < {stop}', baca.leaves()),
+        baca.rruns(),
         )
--- faberge/tools/niente_swell.py
+++ faberge/tools/niente_swell.py
@@ -3,7 +3,7 @@ import baca
 
 
 def niente_swell(dynamic):
-    rtleaves = baca.select().tleaves().rleak()
+    rtleaves = baca.tleaves().rleak()
     return baca.suite([
         baca.hairpin(f'niente < {dynamic}', rtleaves[:2]),
         baca.hairpin(f'{dynamic} > niente', rtleaves[-2:]),
