9c20a9a09e8228ffe27dba7c1b8db198f667c042
Trevor Bača <trevor.baca@gmail.com>
2017-12-13 11:38:21 -0600

    Rerendered materials.

--- faberge/materials/metronome_marks/definition.py
+++ faberge/materials/metronome_marks/definition.py
@@ -4,30 +4,30 @@ import abjad
 metronome_marks = abjad.MetronomeMarkDictionary([
     (
         '41',
-        abjad.MetronomeMark(abjad.Duration(1, 4), 41),
+        abjad.MetronomeMark((1, 4), 41),
         ),
     (
         '52',
-        abjad.MetronomeMark(abjad.Duration(1, 4), 52),
+        abjad.MetronomeMark((1, 4), 52),
         ),
     (
         '64',
-        abjad.MetronomeMark(abjad.Duration(1, 4), 64),
+        abjad.MetronomeMark((1, 4), 64),
         ),
     (
         '80',
-        abjad.MetronomeMark(abjad.Duration(1, 4), 80),
+        abjad.MetronomeMark((1, 4), 80),
         ),
     (
         '100',
-        abjad.MetronomeMark(abjad.Duration(1, 4), 100),
+        abjad.MetronomeMark((1, 4), 100),
         ),
     (
         '125',
-        abjad.MetronomeMark(abjad.Duration(1, 4), 125),
+        abjad.MetronomeMark((1, 4), 125),
         ),
     (
         '156',
-        abjad.MetronomeMark(abjad.Duration(1, 4), 156),
+        abjad.MetronomeMark((1, 4), 156),
         ),
     ])
