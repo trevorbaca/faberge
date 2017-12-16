15d5aba28df1afb57fde8bb5c5fcaca888437c26
Trevor Bača <trevor.baca@gmail.com>
2017-12-16 10:45:27 -0600

    Cleaning up metadata.

--- faberge/segments/A/__metadata__.py
+++ faberge/segments/A/__metadata__.py
@@ -4,7 +4,7 @@ import abjad
 metadata = abjad.TypedOrderedDict(
     [
         (
-            'end_clefs_by_context',
+            'end_clefs',
             abjad.TypedOrderedDict(
                 [
                     (
@@ -48,7 +48,7 @@ metadata = abjad.TypedOrderedDict(
             ),
         ('end_clock_time', "2'56''"),
         (
-            'end_dynamics_by_context',
+            'end_dynamics',
             abjad.TypedOrderedDict(
                 [
                     ('CelloMusicVoice', 'ppp'),
@@ -59,7 +59,7 @@ metadata = abjad.TypedOrderedDict(
                 ),
             ),
         (
-            'end_instruments_by_context',
+            'end_instruments',
             abjad.TypedOrderedDict(
                 [
                     ('CelloMusicStaff', 'cello'),
@@ -75,7 +75,7 @@ metadata = abjad.TypedOrderedDict(
             ),
         ('end_metronome_mark', '100'),
         (
-            'end_staff_lines_by_staff',
+            'end_staff_lines',
             abjad.TypedOrderedDict(
                 [
                     ('EnglishHornMusicStaff', 1),
