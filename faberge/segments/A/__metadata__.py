749f22805930c16414b7780e75e86b3e207e820c
Trevor Bača <trevor.baca@gmail.com>
2018-01-09 13:34:31 -0600

    Wrote spacing section metadata to disk.

--- faberge/segments/A/__metadata__.py
+++ faberge/segments/A/__metadata__.py
@@ -177,6 +177,11 @@ metadata = abjad.TypedOrderedDict(
                                 prototype='abjad.TimeSignature',
                                 value='4/4',
                                 ),
+                            abjad.Momento(
+                                context='GlobalSkips',
+                                prototype='baca.SpacingSection',
+                                value='1/16',
+                                ),
                             ],
                         ),
                     (
--- faberge/segments/_/__metadata__.py
+++ faberge/segments/_/__metadata__.py
@@ -112,6 +112,11 @@ metadata = abjad.TypedOrderedDict(
                                 prototype='abjad.TimeSignature',
                                 value='1/4',
                                 ),
+                            abjad.Momento(
+                                context='GlobalSkips',
+                                prototype='baca.SpacingSection',
+                                value='1/4',
+                                ),
                             ],
                         ),
                     (
