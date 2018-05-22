abd7853bae6c84534defbab051b7c6370091217c
Trevor Bača <trevor.baca@gmail.com>
2018-05-22 18:46:43 -0500

    Refactored mapped expressions.

--- faberge/tools/increasing_dal_niente_hairpins.py
+++ faberge/tools/increasing_dal_niente_hairpins.py
@@ -5,19 +5,19 @@ import baca
 def increasing_dal_niente_hairpins():
     return [
         baca.map(
-            baca.hairpin('niente < p', selector=baca.leaves()),
             baca.rrun(0),
+            baca.hairpin('niente < p', selector=baca.leaves()),
             ),
         baca.map(
-            baca.hairpin('niente < mp', selector=baca.leaves()),
             baca.rrun(1),
+            baca.hairpin('niente < mp', selector=baca.leaves()),
             ),
         baca.map(
-            baca.hairpin('niente < mf', selector=baca.leaves()),
             baca.rruns()[2:4],
+            baca.hairpin('niente < mf', selector=baca.leaves()),
             ),
         baca.map(
-            baca.hairpin('niente < f', selector=baca.leaves()),
             baca.rruns()[4:],
+            baca.hairpin('niente < f', selector=baca.leaves()),
             ),
         ]
