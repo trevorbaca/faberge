10e3b687fc22c9573b2cc2c8322926abbb9342af
Trevor Bača <trevor.baca@gmail.com>
2018-05-31 13:59:24 -0500

    Cleaned up Travis configfile.

--- setup.py
+++ setup.py
@@ -2,14 +2,29 @@
 import setuptools
 
 
+install_requires = [
+    'abjad',
+    'mypy',
+    'roman',
+    ]
+
+keywords = [
+    'abjad',
+    'music composition',
+    'music notation',
+    'formalized score control',
+    'lilypond',
+    ]
+keywords = ', '.join(keywords)
+
 if __name__ == '__main__':
     setuptools.setup(
         author='Trevor Bača',
         author_email='trevor.baca@gmail.com',
-        install_requires=('abjad',),
-        name='faberge',
-        packages=('faberge',),
+        install_requires=install_requires,
+        keywords=keywords,
+        name='Fabergé Investigations',
+        packages=['faberge'],
+        platforms='Any',
         url='https://github.com/trevorbaca/faberge',
-        version='0.1',
-        zip_safe=False,
         )
