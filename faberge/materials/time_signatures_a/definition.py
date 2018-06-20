5a104dd1de19ce52ddf3e6670b13096530c8d340
Trevor Bača <trevor.baca@gmail.com>
2018-06-20 14:25:26 -0500

    Ported helianthation.

--- faberge/materials/time_signatures_a/definition.py
+++ faberge/materials/time_signatures_a/definition.py
@@ -2,8 +2,8 @@ import abjad
 import baca
 
 
-numerators = [[4, 6, 6], [4, 7], [3, 4, 6]]
-numerator_groups = baca.helianthate(numerators, -1, 1)
+numerators = baca.sequence([[4, 6, 6], [4, 7], [3, 4, 6]])
+numerator_groups = numerators.helianthate(-1, 1)
 assert len(numerator_groups) == 18, repr(len(numerator_groups))
 lengths = [len(_) for _ in numerator_groups]
 numerators = baca.sequence(numerator_groups).flatten()
--- faberge/materials/time_signatures_b/definition.py
+++ faberge/materials/time_signatures_b/definition.py
@@ -2,8 +2,8 @@ import abjad
 import baca
 
 
-numerators = [[3, 4, 4], [2, 3], [2, 3, 4]]
-numerator_groups = baca.helianthate(numerators, -1, 1)
+numerators = baca.sequence([[3, 4, 4], [2, 3], [2, 3, 4]])
+numerator_groups = numerators.helianthate(-1, 1)
 assert len(numerator_groups) == 18, repr(len(numerator_groups))
 lengths = [len(_) for _ in numerator_groups]
 numerators = baca.sequence(numerator_groups).flatten()
