1723d9d13fe20b2e5444de0a4310ddc1eeffa21f
Trevor Bača <trevor.baca@gmail.com>
2018-06-25 09:51:49 -0500

    Cleaned up dynamics.

--- faberge/tools/increasing_dal_niente_hairpins.py
+++ faberge/tools/increasing_dal_niente_hairpins.py
@@ -9,18 +9,38 @@ def increasing_dal_niente_hairpins():
     return [
         baca.map(
             baca.run(0),
-            baca.new_hairpin('niente o< p', stop_selector=baca.rleaf(-1)),
+            baca.new_hairpin(
+                'niente o< p',
+                start_selector=baca.pleaf(0),
+                stop_selector=baca.rleaf(-1),
+                trend_selector=baca.pleaf(0),
+                ),
             ),
         baca.map(
             baca.run(1),
-            baca.new_hairpin('niente o< mp', stop_selector=baca.rleaf(-1)),
+            baca.new_hairpin(
+                'niente o< mp',
+                start_selector=baca.pleaf(0),
+                stop_selector=baca.rleaf(-1),
+                trend_selector=baca.pleaf(0),
+                ),
             ),
         baca.map(
             baca.runs()[2:4],
-            baca.new_hairpin('niente o< mf', stop_selector=baca.rleaf(-1)),
+            baca.new_hairpin(
+                'niente o< mf',
+                start_selector=baca.pleaf(0),
+                stop_selector=baca.rleaf(-1),
+                trend_selector=baca.pleaf(0),
+                ),
             ),
         baca.map(
             baca.runs()[4:],
-            baca.new_hairpin('niente o< f', stop_selector=baca.rleaf(-1)),
+            baca.new_hairpin(
+                'niente o< f',
+                start_selector=baca.pleaf(0),
+                stop_selector=baca.rleaf(-1),
+                trend_selector=baca.pleaf(0),
+                ),
             ),
         ]
