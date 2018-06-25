d1046767023ad8c136e6990010c0f80bda5c046d
Trevor Bača <trevor.baca@gmail.com>
2018-06-25 16:50:47 -0500

    Cleaned up dynamics.

--- faberge/tools/dal_niente_hairpins.py
+++ faberge/tools/dal_niente_hairpins.py
@@ -6,11 +6,9 @@ def dal_niente_hairpins(stop):
     Makes dal niente hairpins.
     """
     return baca.map(
-        baca.runs(),
+        baca.rruns(),
         baca.new_hairpin(
             f'niente o< {stop}',
-            start_selector=baca.pleaf(0),
-            stop_selector=baca.rleaf(-1),
-            trend_selector=baca.pleaf(0),
+            stop_selector=baca.rest(-1),
             ),
         )
--- faberge/tools/increasing_dal_niente_hairpins.py
+++ faberge/tools/increasing_dal_niente_hairpins.py
@@ -8,39 +8,31 @@ def increasing_dal_niente_hairpins():
     """
     return [
         baca.map(
-            baca.run(0),
+            baca.rrun(0),
             baca.new_hairpin(
                 'niente o< p',
-                start_selector=baca.pleaf(0),
-                stop_selector=baca.rleaf(-1),
-                trend_selector=baca.pleaf(0),
+                stop_selector=baca.rest(-1),
                 ),
             ),
         baca.map(
-            baca.run(1),
+            baca.rrun(1),
             baca.new_hairpin(
                 'niente o< mp',
-                start_selector=baca.pleaf(0),
-                stop_selector=baca.rleaf(-1),
-                trend_selector=baca.pleaf(0),
+                stop_selector=baca.rest(-1),
                 ),
             ),
         baca.map(
-            baca.runs()[2:4],
+            baca.rruns()[2:4],
             baca.new_hairpin(
                 'niente o< mf',
-                start_selector=baca.pleaf(0),
-                stop_selector=baca.rleaf(-1),
-                trend_selector=baca.pleaf(0),
+                stop_selector=baca.rest(-1),
                 ),
             ),
         baca.map(
-            baca.runs()[4:],
+            baca.rruns()[4:],
             baca.new_hairpin(
                 'niente o< f',
-                start_selector=baca.pleaf(0),
-                stop_selector=baca.rleaf(-1),
-                trend_selector=baca.pleaf(0),
+                stop_selector=baca.rest(-1),
                 ),
             ),
         ]
