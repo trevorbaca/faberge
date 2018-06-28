2409a64832a85911003fccfe6e5eb338c66d8593
Trevor Bača <trevor.baca@gmail.com>
2018-06-28 14:51:37 -0500

    Cleaned up dynamics.

--- faberge/tools/niente_swells.py
+++ faberge/tools/niente_swells.py
@@ -7,51 +7,8 @@ def niente_swells(dynamic):
     Makes niente swells.
     """
     assert isinstance(dynamic, str), repr(dynamic)
-
-    commands = []
-
-    selector = baca.select().ntruns()
-    selector = selector.map(baca.select().pleaf(0))
-    command = baca.dynamic(
-        baca.niente(),
-        selector=selector,
+    return baca.map(
+        baca.ntruns(),
+        baca.hairpin(f'niente o< {dynamic}', selector=baca.tleaves()[:2]),
+        baca.hairpin(f'{dynamic} >o niente', selector=baca.rleaves()[-2:]),
         )
-    commands.append(command)
-    command = baca.dynamic_trend(
-        'o<',
-        selector=selector,
-        )
-    commands.append(command)
-
-    selector = baca.select().ntruns()
-    selector = selector.map(baca.select().pleaf(1))
-    command = baca.dynamic(
-        dynamic,
-        selector=selector,
-        )
-    commands.append(command)
-
-    selector = baca.select().ntruns()
-    selector = selector.map(baca.select().pleaf(-1))
-    command = baca.dynamic(
-        dynamic,
-        selector=selector,
-        )
-    commands.append(command)
-    dynamic_trend = abjad.DynamicTrend('>o')
-    abjad.tweak(dynamic_trend).to_barline = True
-    command = baca.dynamic_trend(
-        dynamic_trend,
-        selector=selector,
-        )
-    commands.append(command)
-
-    selector = baca.select().ntruns()
-    selector = selector.map(baca.select().rleaf(-1))
-    command = baca.dynamic(
-        baca.niente(),
-        selector=selector,
-        )
-    commands.append(command)
-
-    return commands
