8127ae696a14ae34c7b38504a1c81ad40dea50ab
Trevor Bača <trevor.baca@gmail.com>
2018-12-14 09:39:57 -0600

    O.

---
+++ faberge/segments/M/definition.py
@@ -0,0 +1,25 @@
+import abjad
+import baca
+import faberge
+import os
+
+
+###############################################################################
+##################################### [M] #####################################
+###############################################################################
+
+maker = baca.SegmentMaker(
+    phantom=True,
+    segment_directory=abjad.Path(os.path.realpath(__file__)).parent,
+    time_signatures=[(4, 4)],
+    transpose_score=False,
+    validate_measure_count=1,
+    )
+
+maker(
+    'Global_Skips',
+    baca.rehearsal_mark(
+        'M',
+        abjad.tweak((0, 18)).extra_offset,
+        ),
+    )
---
+++ faberge/stylesheets/markups.ily
@@ -0,0 +1,13 @@
+\include "/Users/trevorbaca/baca/lilypond/baca.ily"
+
+%%% COLOPHON %%%
+
+faberge-colophon-markup = \markup {
+    \override #'(font-name . "Palatino")
+    \with-color #black
+    \right-column {
+        \line { Madison, WI. }
+        \line { Mar. \hspace #0.75 – \hspace #0.75 Apr. 2016. }
+        \line { Oct. \hspace #0.75 – \hspace #0.75 Dec. 2018. }
+        }
+    }
