0b79e6dbe28163ce3cb27e2c50c049fecb7e18f1
Trevor Bača <trevor.baca@gmail.com>
2018-12-20 13:39:42 -0600

    M.

--- faberge/segments/L/__metadata__.py
+++ faberge/segments/L/__metadata__.py
@@ -91,7 +91,7 @@ metadata = abjad.OrderedDict(
                             abjad.Momento(
                                 context='Cello_Music_Voice',
                                 prototype='baca.StaffLines',
-                                value=1,
+                                value=5,
                                 ),
                             ],
                         ),
@@ -322,7 +322,7 @@ metadata = abjad.OrderedDict(
                             abjad.Momento(
                                 context='Viola_Music_Voice',
                                 prototype='baca.StaffLines',
-                                value=1,
+                                value=5,
                                 ),
                             ],
                         ),
@@ -370,7 +370,7 @@ metadata = abjad.OrderedDict(
                             abjad.Momento(
                                 context='Violin_Music_Voice',
                                 prototype='baca.StaffLines',
-                                value=1,
+                                value=5,
                                 ),
                             ],
                         ),
