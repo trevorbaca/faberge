5cac82d06cb20047a94f4fe4162b0254ec80e487
Trevor Bača <trevor.baca@gmail.com>
2018-12-26 15:42:24 -0600

    F.

--- faberge/segments/O/__metadata__.py
+++ faberge/segments/O/__metadata__.py
@@ -48,7 +48,7 @@ metadata = abjad.OrderedDict(
             ),
         (
             'bol_measure_numbers',
-            [280],
+            [279],
             ),
         ('duration', "0'23''"),
         ('final_measure_number', 286),
