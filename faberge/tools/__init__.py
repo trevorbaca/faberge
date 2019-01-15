592c4433dbb2f334958716bf8fd4044e1e592997
Trevor Bača <trevor.baca@gmail.com>
2019-01-14 20:44:10 -0600

    A.

--- faberge/tools/__init__.py
+++ faberge/tools/__init__.py
@@ -12,6 +12,7 @@ from .eh_trill_rhythm import eh_trill_rhythm
 from .end_of_cell_attack import end_of_cell_attack
 from .even_tuplet_rhythm import even_tuplet_rhythm
 from .front_incised_divisions import front_incised_divisions
+from .fused_divisions import fused_divisions
 from .glow_rhythm import glow_rhythm
 from .halves_rhythm import halves_rhythm
 from .increasing_dal_niente_hairpins import increasing_dal_niente_hairpins
--- faberge/tools/bfl_color_fingerings.py
+++ faberge/tools/bfl_color_fingerings.py
@@ -1,9 +1,14 @@
 import baca
 
 
-def bfl_color_fingerings():
+def bfl_color_fingerings(
+    *tweaks,
+    ):
     """
     Makes bass flute color fingerings.
     """
 
-    return baca.color_fingerings([0, 1, 2, 1, 0, 1, 0, 1, 2, 1, 2, 1])
+    return baca.color_fingerings(
+        [0, 1, 2, 1, 0, 1, 0, 1, 2, 1, 2, 1],
+        *tweaks,
+        )
