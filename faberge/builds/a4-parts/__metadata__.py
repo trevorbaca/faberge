20cbfde7de5e9da851ade604e2454977d507957b
Trevor Bača <trevor.baca@gmail.com>
2019-01-18 00:51:50 -0500

    Building parts.

---
+++ faberge/builds/a4-parts/__metadata__.py
@@ -0,0 +1,10 @@
+import abjad
+
+
+metadata = abjad.OrderedDict(
+    [
+        ('catalog_number_suffix', 'A4'),
+        ('paper_size', 'a4'),
+        ('parts_directory', True),
+        ]
+    )
