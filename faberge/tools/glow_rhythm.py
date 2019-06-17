ae1303125dbf803579ec923eb4facc19b07133cc
Trevor Bača <trevor.baca@gmail.com>
2019-06-17 13:33:19 -0500

    Cleaned up tuplet specifiers.

--- faberge/tools/glow_rhythm.py
+++ faberge/tools/glow_rhythm.py
@@ -25,15 +25,15 @@ def glow_rhythm(
         rewrite_meter=True,
         rewrite_rest_filled=True,
         rhythm_maker=rmakers.TupletRhythmMaker(
+            rmakers.TupletSpecifier(
+                extract_trivial=True, rewrite_rest_filled=True, trivialize=True
+            ),
             rmakers.TieSpecifier(
                 tie_across_divisions=True, repeat_ties=(1, 2)
             ),
             rmakers.BeamSpecifier(beam_each_division=True),
             division_masks=dmask,
             tuplet_ratios=tuplet_ratios_,
-            tuplet_specifier=rmakers.TupletSpecifier(
-                extract_trivial=True, rewrite_rest_filled=True, trivialize=True
-            ),
         ),
         tag="faberge.glow_rhythm",
     )
