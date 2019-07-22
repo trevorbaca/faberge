5393bb6ba2655cf3aca27dcfc25d86f116113c38
Trevor Bača <trevor.baca@gmail.com>
2019-07-22 15:47:56 -0400

    Cleaned up rhythm construction.

--- faberge/tools/ratchet_rhythm.py
+++ faberge/tools/ratchet_rhythm.py
@@ -9,10 +9,7 @@ def ratchet_rhythm() -> baca.RhythmCommand:
     """
     return baca.rhythm(
         rmakers.accelerando(
-            interpolations=[
-                rmakers.interpolate((3, 8), (1, 16), (1, 16)),
-                rmakers.interpolate((1, 16), (3, 8), (1, 16)),
-            ]
+            [(3, 8), (1, 16), (1, 16)], [(1, 16), (3, 8), (1, 16)]
         ),
         rmakers.duration_bracket(),
         rmakers.feather_beam(beam_rests=True, stemlet_length=0.75),
