5f3b4b3e13c21d81801a52a7c9fa5fb1cbe03b37
Trevor Bača <trevor.baca@gmail.com>
2020-08-27 13:07:54 -0400

    Blacked code.

--- faberge/segments/I/__metadata__.py
+++ faberge/segments/I/__metadata__.py
@@ -4,28 +4,28 @@ import abjad
 metadata = abjad.OrderedDict(
     [
         (
-            'bol_measure_numbers',
+            "bol_measure_numbers",
             [229],
-            ),
-        ('duration', "0'22''"),
-        ('final_measure_number', 236),
-        ('first_measure_number', 229),
-        ('segment_name', 'I'),
-        ('segment_number', 10),
-        ('start_clock_time', "9'22''"),
-        ('stop_clock_time', "9'44''"),
+        ),
+        ("duration", "0'22''"),
+        ("final_measure_number", 236),
+        ("first_measure_number", 229),
+        ("segment_name", "I"),
+        ("segment_number", 10),
+        ("start_clock_time", "9'22''"),
+        ("stop_clock_time", "9'44''"),
         (
-            'time_signatures',
+            "time_signatures",
             [
-                '6/4',
-                '6/4',
-                '6/4',
-                '4/4',
-                '9/4',
-                '6/4',
-                '6/4',
-                '5/4',
-                ],
-            ),
-        ]
-    )
+                "6/4",
+                "6/4",
+                "6/4",
+                "4/4",
+                "9/4",
+                "6/4",
+                "6/4",
+                "5/4",
+            ],
+        ),
+    ]
+)
