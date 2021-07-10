d8cfba414dfd4d609efd6e6c401ac0cd5db521ef
Trevor Bača <trevor.baca@gmail.com>
2021-07-10 15:26:30 -0400

    Cleaned up fermata measure number metadata.

--- faberge/__metadata__.py
+++ faberge/__metadata__.py
@@ -4,37 +4,6 @@ metadata = abjad.OrderedDict(
     [
         ("catalog_number", "AWN-015"),
         ("composer", "Trevor Bača"),
-        (
-            "fermata_measure_numbers",
-            abjad.OrderedDict(
-                [
-                    (
-                        "01",
-                        [2, 4],
-                    ),
-                    (
-                        "05",
-                        [193],
-                    ),
-                    (
-                        "06",
-                        [196],
-                    ),
-                    (
-                        "08",
-                        [211, 216],
-                    ),
-                    (
-                        "09",
-                        [227],
-                    ),
-                    (
-                        "15",
-                        [278],
-                    ),
-                ]
-            ),
-        ),
         ("forces_tagline", "for eight players"),
         ("title", "Fabergé Investigations"),
         ("year", "2016/19"),
