b19b49d218265f696e16a726dd8959def4e95c35
Trevor Bača <trevor.baca@gmail.com>
2021-09-23 11:52:23 +0200

    Cleaned up definition files.

--- faberge/segments/02/music.py
+++ faberge/segments/02/music.py
@@ -36,6 +36,7 @@ maker_ = baca.TimeSignatureMaker(
 time_signatures = maker_.run()
 
 maker = baca.SegmentMaker(
+    **baca.segments(),
     activate=[
         baca.tags.LOCAL_MEASURE_NUMBER,
         baca.tags.STAGE_NUMBER,
