fa5eb7ddcae4cae8cdbbe1a84e1c8adb295c1050
Trevor Bača <trevor.baca@gmail.com>
2022-05-05 10:18:28 +0200

    Cleaned up music.py files.

--- faberge/sections/03/music.py
+++ faberge/sections/03/music.py
@@ -1300,7 +1300,7 @@ commands(
     baca.marcato(
         selector=lambda _: baca.select.pheads(_),
     ),
-    library.piano_clusters(),
+    library.replace_with_piano_clusters(),
 )
 
 commands(
