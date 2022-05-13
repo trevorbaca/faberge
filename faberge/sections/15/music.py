ebeb83f90e002764d28481f12f663cee16854cd3
Trevor Bača <trevor.baca@gmail.com>
2022-05-13 16:00:59 +0200

    Cleaned up  music.py files.

--- faberge/sections/15/music.py
+++ faberge/sections/15/music.py
@@ -529,7 +529,7 @@ commands(
 )
 
 if __name__ == "__main__":
-    metadata, persist, score, timing = baca.build.interpret_segment(
+    metadata, persist, score, timing = baca.build.interpret_section(
         score,
         commands,
         **baca.score_interpretation_defaults(),
@@ -538,12 +538,9 @@ if __name__ == "__main__":
             baca.tags.STAGE_NUMBER,
         ),
         always_make_global_rests=True,
-        append_phantom_measures_by_hand=True,
-        do_not_sort_commands=True,
         error_on_not_yet_pitched=True,
         fermata_measure_empty_overrides=[9],
         global_rests_in_topmost_staff=True,
-        intercalate_mmrests_by_hand=True,
         stage_markup=stage_markup,
         transpose_score=True,
     )
