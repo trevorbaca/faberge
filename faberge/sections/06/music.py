e850f3b64fe261d8c214f1480264079eb0de9afe
Trevor Bača <trevor.baca@gmail.com>
2022-06-05 19:58:45 +0200

    Cleaned up music.py files.

--- faberge/sections/06/music.py
+++ faberge/sections/06/music.py
@@ -33,6 +33,16 @@ commands = baca.CommandAccumulator(
     voice_names=voice_names,
 )
 
+baca.interpret.set_up_score(
+    score,
+    commands.manifests(),
+    commands.time_signatures,
+    append_anchor_skip=True,
+    always_make_global_rests=True,
+    attach_nonfirst_empty_start_bar=True,
+    stage_markup=stage_markup,
+)
+
 commands(
     "Skips",
     baca.chunk(
@@ -865,7 +875,6 @@ if __name__ == "__main__":
         error_on_not_yet_pitched=True,
         fermata_measure_empty_overrides=[3],
         global_rests_in_topmost_staff=True,
-        stage_markup=stage_markup,
         transpose_score=True,
     )
     lilypond_file = baca.make_lilypond_file(
