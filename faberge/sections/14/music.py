3a18a1fe498b3a1729015ed29bad6d98cf9ed871
Trevor Bača <trevor.baca@gmail.com>
2022-08-16 08:03:06 -0400

    Bumped LilyPond to 2.23.11.

--- faberge/sections/14/music.py
+++ faberge/sections/14/music.py
@@ -539,7 +539,7 @@ def main():
     VN(accumulator.voice("vn"))
     VA(accumulator.voice("va"))
     VC(accumulator.voice("vc"))
-    previous_persist = baca.previous_metadata(__file__, file_name="__persist__")
+    previous_persist = baca.previous_persist(__file__)
     voice_metadata = previous_persist.get("voice_metadata", {})
     baca.reapply(accumulator, accumulator.manifests(), previous_persist, voice_names)
     cache = baca.interpret.cache_leaves(
