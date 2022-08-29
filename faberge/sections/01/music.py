4016577c4dc3bf50f3485821af24b7be770a186f
Trevor Bača <trevor.baca@gmail.com>
2022-08-29 08:01:31 -0400

    Cleaned up music.py files.

--- faberge/sections/01/music.py
+++ faberge/sections/01/music.py
@@ -270,10 +270,10 @@ def main():
         library.manifests,
         accumulator.time_signatures,
         **baca.interpret.section_defaults(),
-        activate=(
+        activate=[
             baca.tags.LOCAL_MEASURE_NUMBER,
             baca.tags.STAGE_NUMBER,
-        ),
+        ],
         always_make_global_rests=True,
         empty_fermata_measures=True,
         error_on_not_yet_pitched=True,
