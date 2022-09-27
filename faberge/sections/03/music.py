e30f5cd36f084147be8ead541ffcc7a04f61e163
Trevor Bača <trevor.baca@gmail.com>
2022-09-27 10:25:37 -0400

    Cleaned up metadata.

--- faberge/sections/03/music.py
+++ faberge/sections/03/music.py
@@ -1028,8 +1028,8 @@ def main():
     )
     baca.build.persist_lilypond_file(
         lilypond_file,
-        environment.metadata,
-        environment.persist,
+        metadata,
+        persist,
         timing,
         environment.arguments,
     )
--- faberge/sections/09/music.py
+++ faberge/sections/09/music.py
@@ -781,8 +781,8 @@ def main():
     )
     baca.build.persist_lilypond_file(
         lilypond_file,
-        environment.metadata,
-        environment.persist,
+        metadata,
+        persist,
         timing,
         environment.arguments,
     )
