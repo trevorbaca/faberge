6c30d5978b5af29ac0ca81f04c9326eaf5aa9452
Trevor Bača <trevor.baca@gmail.com>
2022-10-03 06:17:41 -0400

    Cleaned up timing.

--- faberge/sections/05/music.py
+++ faberge/sections/05/music.py
@@ -439,12 +439,11 @@ def make_score(
 
 def main():
     environment = baca.build.read_environment(__file__, baca.build.argv())
-    timing = baca.build.Timing()
     score, measures, voice_name_to_parameter_to_state = make_score(
         environment.first_measure_number,
         environment.previous_persist["persistent_indicators"],
         environment.previous_persist["voice_name_to_parameter_to_state"],
-        timing,
+        environment.timing,
     )
     metadata, persist = baca.section.postprocess_score(
         score,
@@ -460,7 +459,6 @@ def main():
         error_on_not_yet_pitched=True,
         global_rests_in_topmost_staff=True,
         manifests=library.manifests,
-        timing=timing,
         transpose_score=True,
     )
     dictionary = dict(persist)
@@ -475,7 +473,7 @@ def main():
         lilypond_file,
         metadata,
         persist,
-        timing,
+        environment.timing,
         environment.arguments,
     )
 
--- faberge/sections/16/music.py
+++ faberge/sections/16/music.py
@@ -275,11 +275,10 @@ def make_score(first_measure_number, previous_persistent_indicators):
 
 def main():
     environment = baca.build.read_environment(__file__, baca.build.argv())
-    timing = baca.build.Timing()
     score, measures = make_score(
         environment.first_measure_number,
         environment.previous_persist["persistent_indicators"],
-        timing,
+        environment.timing,
     )
     metadata, persist = baca.section.postprocess_score(
         score,
@@ -296,7 +295,6 @@ def main():
         final_section=True,
         global_rests_in_topmost_staff=True,
         manifests=library.manifests,
-        timing=timing,
         transpose_score=True,
     )
     lilypond_file = baca.lilypond.file(
@@ -308,7 +306,7 @@ def main():
         lilypond_file,
         metadata,
         persist,
-        timing,
+        environment.timing,
         environment.arguments,
     )
 
