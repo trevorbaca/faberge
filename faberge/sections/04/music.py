8a5780c7122621aa03d96910a266aff8fd79a966
Trevor Bača <trevor.baca@gmail.com>
2022-10-03 06:17:41 -0400

    Cleaned up timing.

--- faberge/sections/04/music.py
+++ faberge/sections/04/music.py
@@ -497,11 +497,10 @@ def make_score(first_measure_number, previous_persistent_indicators):
 
 def main():
     environment = baca.build.read_environment(__file__, baca.build.argv())
-    timing = baca.build.Timing()
     score, measures, voice_name_to_parameter_to_state = make_score(
         environment.first_measure_number,
         environment.previous_persist["persistent_indicators"],
-        timing,
+        environment.timing,
     )
     metadata, persist = baca.section.postprocess_score(
         score,
@@ -517,7 +516,6 @@ def main():
         error_on_not_yet_pitched=True,
         global_rests_in_topmost_staff=True,
         manifests=library.manifests,
-        timing=timing,
         transpose_score=True,
     )
     dictionary = dict(persist)
@@ -532,7 +530,7 @@ def main():
         lilypond_file,
         metadata,
         persist,
-        timing,
+        environment.timing,
         environment.arguments,
     )
 
