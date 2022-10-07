9c1902555a2cd3ff034ebbab66765fa328b074b4
Trevor Bača <trevor.baca@gmail.com>
2022-10-07 09:12:46 -0400

    Cleaned up music.py files.

--- faberge/sections/01/music.py
+++ faberge/sections/01/music.py
@@ -262,7 +262,6 @@ def main():
     score = make_score(environment.timing)
     metadata = baca.section.postprocess_score(
         score,
-        **baca.section.section_defaults(),
         always_make_global_rests=True,
         empty_fermata_measures=True,
         environment=environment,
