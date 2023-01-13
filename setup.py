1ce7c4d7d241f7d097eda0b5449b8e60cf8fd25b
Trevor Bača <trevor.baca@gmail.com>
2023-01-13 18:17:28 -0500

    Set python_requires>=3.10.

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
         name="faberge",
         packages=["faberge"],
         platforms="Any",
-        python_requires=">=3.11",
+        python_requires=">=3.10",
         url="https://github.com/trevorbaca/faberge",
     )
