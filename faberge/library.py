0d6996243dfa54b53905e37062e1c784904e45bc
Trevor Bača <trevor.baca@gmail.com>
2023-04-15 09:44:47 -0400

    Cleaned up metronome marks.

--- faberge/library.py
+++ faberge/library.py
@@ -871,13 +871,13 @@ instruments = {
 
 
 metronome_marks = {
-    "41": abjad.MetronomeMark((1, 4), 41),
-    "51": abjad.MetronomeMark((1, 4), 51),
-    "64": abjad.MetronomeMark((1, 4), 64),
-    "80": abjad.MetronomeMark((1, 4), 80),
-    "100": abjad.MetronomeMark((1, 4), 100),
-    "125": abjad.MetronomeMark((1, 4), 125),
-    "156": abjad.MetronomeMark((1, 4), 156),
+    "41": abjad.MetronomeMark(abjad.Duration(1, 4), 41),
+    "51": abjad.MetronomeMark(abjad.Duration(1, 4), 51),
+    "64": abjad.MetronomeMark(abjad.Duration(1, 4), 64),
+    "80": abjad.MetronomeMark(abjad.Duration(1, 4), 80),
+    "100": abjad.MetronomeMark(abjad.Duration(1, 4), 100),
+    "125": abjad.MetronomeMark(abjad.Duration(1, 4), 125),
+    "156": abjad.MetronomeMark(abjad.Duration(1, 4), 156),
     # slower
     "4:5(4)=4": abjad.MetricModulation(
         left_rhythm=abjad.Tuplet("4:5", "c4"),
