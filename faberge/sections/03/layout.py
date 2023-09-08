e3e648afe44d3a82a39295fba676ba4999b19ff7
Trevor Bača <trevor.baca@gmail.com>
2023-09-08 13:15:48 -0400

    Cleaned up layout.py files.

--- faberge/sections/03/layout.py
+++ faberge/sections/03/layout.py
@@ -2,26 +2,29 @@ import math
 
 import baca
 
-distances = [18, (21, 21, 24), (21, 21, 24), (24, 24, 24)]
-first_measure_number = 97
-final_measure_number = 176
-measure_count = final_measure_number - first_measure_number + 1
-measures_per_page = 7
-page_count = math.ceil(measure_count / measures_per_page)
-pages = []
-for page_index in range(page_count):
-    page_number = page_index + 1
-    start_measure_number = measures_per_page * page_index + 1
-    page = baca.page(
-        page_number,
-        baca.system(measure=start_measure_number, y_offset=40, distances=distances),
-    )
-    pages.append(page)
 
-spacing = baca.make_layout(
-    *pages,
-    spacing=(1, 48),
-)
+def main():
+    distances = [18, (21, 21, 24), (21, 21, 24), (24, 24, 24)]
+    first_measure_number = 97
+    final_measure_number = 176
+    measure_count = final_measure_number - first_measure_number + 1
+    measures_per_page = 7
+    page_count = math.ceil(measure_count / measures_per_page)
+    pages = []
+    for page_index in range(page_count):
+        page_number = page_index + 1
+        start_measure_number = measures_per_page * page_index + 1
+        page = baca.page(
+            page_number,
+            baca.system(measure=start_measure_number, y_offset=40, distances=distances),
+        )
+        pages.append(page)
+    spacing = baca.make_layout(
+        *pages,
+        spacing=(1, 48),
+    )
+    baca.section.make_layout_ly(spacing)
+
 
 if __name__ == "__main__":
-    baca.section.make_layout_ly(spacing)
+    main()
--- faberge/sections/07/layout.py
+++ faberge/sections/07/layout.py
@@ -2,17 +2,21 @@ import baca
 
 distances = [18, (21, 21, 24), (21, 21, 24), (21, 21, 24)]
 
-spacing = baca.make_layout(
-    baca.page(
-        1,
-        baca.system(measure=1, y_offset=40, distances=distances),
-    ),
-    spacing=(1, 20),
-    overrides=(
-        baca.space(1, (1, 40)),
-        baca.space((2, 4), (1, 24)),
-    ),
-)
+
+def main():
+    spacing = baca.make_layout(
+        baca.page(
+            1,
+            baca.system(measure=1, y_offset=40, distances=distances),
+        ),
+        spacing=(1, 20),
+        overrides=(
+            baca.space(1, (1, 40)),
+            baca.space((2, 4), (1, 24)),
+        ),
+    )
+    baca.section.make_layout_ly(spacing)
+
 
 if __name__ == "__main__":
-    baca.section.make_layout_ly(spacing)
+    main()
