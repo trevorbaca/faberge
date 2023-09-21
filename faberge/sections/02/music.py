dd89daea98e4bb8751ecb6182a8b3978fd2bc558
Trevor Bača <trevor.baca@gmail.com>
2023-09-21 12:42:39 -0400

    Cleaned up music.py files.

--- faberge/sections/02/music.py
+++ faberge/sections/02/music.py
@@ -1,3 +1,5 @@
+import math
+
 import abjad
 import baca
 
@@ -990,6 +992,34 @@ def main():
         lilypond_file,
         metadata,
     )
+
+
+def make_layout():
+    distances = [18, (21, 21, 24), (21, 21, 24), (21, 21, 24)]
+    first_measure_number = 5
+    final_measure_number = 96
+    measure_count = final_measure_number - first_measure_number + 1
+    measures_per_page = 7
+    page_count = math.ceil(measure_count / measures_per_page)
+    pages = []
+    for page_index in range(page_count):
+        page_number = page_index + 1
+        start_measure_number = measures_per_page * page_index + 1
+        page = baca.page(
+            page_number,
+            baca.system(
+                measure=start_measure_number,
+                y_offset=40,
+                distances=distances,
+            ),
+        )
+        pages.append(page)
+    spacing = baca.make_layout(
+        *pages,
+        spacing=(1, 48),
+        overrides=(baca.space(1, (1, 56)),),
+    )
+    baca.section.make_layout_ly(spacing)
 
 
 if __name__ == "__main__":
--- faberge/sections/06/music.py
+++ faberge/sections/06/music.py
@@ -679,5 +679,17 @@ def main():
     )
 
 
+def make_layout():
+    distances = [18, (21, 21, 24), (21, 21, 24), (24, 24, 24)]
+    spacing = baca.make_layout(
+        baca.page(
+            1,
+            baca.system(measure=1, y_offset=40, distances=distances),
+        ),
+        spacing=(1, 24),
+    )
+    baca.section.make_layout_ly(spacing)
+
+
 if __name__ == "__main__":
     main()
