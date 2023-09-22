1adf887dfc03130f306f87eb312014f0a1ad5188
Trevor Bača <trevor.baca@gmail.com>
2023-09-22 14:32:26 -0400

    Cleaned up music.py files.

--- faberge/sections/01/music.py
+++ faberge/sections/01/music.py
@@ -228,7 +228,6 @@ def make_score():
         score,
         time_signatures(),
         append_anchor_skip=True,
-        always_make_global_rests=True,
         manifests=library.manifests,
     )
     GLOBALS(score["Skips"], score["Rests"])
@@ -261,7 +260,6 @@ def persist_score(score, environment):
         score,
         environment,
         library.manifests,
-        always_make_global_rests=True,
         empty_fermata_measures=True,
         error_on_not_yet_pitched=True,
         first_section=True,
